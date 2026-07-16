cacaa9579490e6ee6ba08ea5973c06206b381dd4
Mariana <mariana_meira_@hotmail.com>
2026-07-16 22:27:41 +1000

    Fix zoom bug - listing data is single object not array

--- src/containers/SearchPage/SearchMap/SearchMapWithMapbox.js
+++ src/containers/SearchPage/SearchMap/SearchMapWithMapbox.js
@@ -397,9 +397,9 @@ class SearchMapWithMapbox extends Component {
       config,
     } = this.props;
     const onListingClickedWithZoom = clickedListings => {
-      console.log('DEBUG clickedListings:', clickedListings, 'this.map exists:', !!this.map);
-      if (clickedListings && clickedListings.length === 1 && this.map) {
-        const geo = clickedListings[0].attributes.geolocation;
+      const listing = Array.isArray(clickedListings) ? clickedListings[0] : clickedListings;
+      if (listing && listing.attributes && listing.attributes.geolocation && this.map) {
+        const geo = listing.attributes.geolocation;
         this.map.flyTo({ center: [geo.lng, geo.lat], zoom: 15 });
       }
       onListingClicked(clickedListings);
